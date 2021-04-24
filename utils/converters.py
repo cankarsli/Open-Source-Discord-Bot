9572c74998eff85131a59fc648cbcfaaf2b64842
Muhammet Can Karslı <59134478+imnewuser@users.noreply.github.com>
2021-04-24 16:59:25 +0300

    Add files via upload

---
+++ utils/converters.py
@@ -0,0 +1,26 @@
+import re
+from datetime import datetime
+
+from discord import User, HTTPException
+
+from discord.ext.commands import BadArgument, UserConverter
+
+class GetFetchUser(UserConverter):
+    async def convert(self, ctx, user: str) -> User:
+        """
+        :param user: str argument coming from Discord.
+        Tries to get user from cache and if fails tries to fetch user from Discord.
+        If both fail raises BadArgument
+        """
+        try:
+            return await super().convert(ctx, user)
+        except BadArgument:
+            try:
+                user_id = int(user)
+            except ValueError:
+                raise BadArgument("Can't convert to neither user nor snowflake.")
+            else:
+                try:
+                    return await ctx.bot.fetch_user(user_id)
+                except HTTPException as e:
+                    raise BadArgument(e)
